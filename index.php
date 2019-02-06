2f4708170905ee736301ca6e55e239ed52f97a50
riazanowa <45009530+riazanowa@users.noreply.github.com>
2019-02-06 08:50:43 +0300

    ПЕренесла массивы

--- index.php
+++ index.php
@@ -3,6 +3,56 @@ $is_auth = rand(0, 1);
 
 $user_name = 'Ryazanova Ekaterina'; // укажите здесь ваше имя
 ?>
+<?php 
+        $pruduct_list = [
+    [
+        'name' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => '10999',
+        'picture_adress' => 'img/lot-1.jpg',
+    ],
+    
+    [
+        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => '159999',
+        'picture_adress' => 'img/lot-2.jpg',
+    ],
+
+    [
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => '8000',
+        'picture_adress' => 'img/lot-3.jpg',
+    ],
+
+    [
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => '10999',
+        'picture_adress' => 'img/lot-4.jpg',
+    ],
+
+    [
+        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price' => '7500',
+        'picture_adress' => 'img/lot-5.jpg',
+    ],
+
+    [
+        'name' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => '5400',
+        'picture_adress' => 'img/lot-6.jpg',
+    ]
+    ];
+?>
+
+<?php
+    $categories = ["Доски и лыжи","Крепления", "Ботинки", "Одежда","Инструменты", "Разное"];
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -60,51 +110,7 @@ $user_name = 'Ryazanova Ekaterina'; // укажите здесь ваше имя
         <div class="lots__header">
             <h2>Открытые лоты</h2>
         </div>
-    <?php 
-        $pruduct_list = [
-    [
-        'name' => '2014 Rossignol District Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => '10999',
-        'picture_adress' => 'img/lot-1.jpg',
-    ],
-    
-    [
-        'name' => 'DC Ply Mens 2016/2017 Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => '159999',
-        'picture_adress' => 'img/lot-2.jpg',
-    ],
-
-    [
-        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'category' => 'Крепления',
-        'price' => '8000',
-        'picture_adress' => 'img/lot-3.jpg',
-    ],
-
-    [
-        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'category' => 'Ботинки',
-        'price' => '10999',
-        'picture_adress' => 'img/lot-4.jpg',
-    ],
-
-    [
-        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'category' => 'Одежда',
-        'price' => '7500',
-        'picture_adress' => 'img/lot-5.jpg',
-    ],
-
-    [
-        'name' => 'Маска Oakley Canopy',
-        'category' => 'Разное',
-        'price' => '5400',
-        'picture_adress' => 'img/lot-6.jpg',
-    ]
-    ];
-    ?>
+  
         <ul class="lots__list">
             <?php foreach ($pruduct_list as $key => $item): ?>
             <!--заполните этот список из массива с товарами-->
@@ -134,9 +140,7 @@ $user_name = 'Ryazanova Ekaterina'; // укажите здесь ваше имя
 
 <footer class="main-footer">
     <nav class="nav">
-    <?php
-    $categories = ["Доски и лыжи","Крепления", "Ботинки", "Одежда","Инструменты", "Разное"];
-    ?>
+
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
         <?php
